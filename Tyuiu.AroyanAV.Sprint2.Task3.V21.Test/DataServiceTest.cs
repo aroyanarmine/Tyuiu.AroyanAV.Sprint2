c3042f850aa9bdb2cbd4e25a5439799ff0562978
aroyanarmine <aroanarmine068@gmail.com>
2025-10-16 13:59:48 +0500

    Task3

---
+++ Tyuiu.AroyanAV.Sprint2.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.AroyanAV.Sprint2.Task3.V21.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint2.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+            {
+                DataService ds = new DataService();
+                double x = 6;
+                double res = ds.Calculate(x);
+                double wait = 4374;
+                Assert.AreEqual(wait, res);
+            }
+
+            [TestMethod]
+            public void TestMethod2()
+            {
+                DataService ds = new DataService();
+                double x = 0;
+                double res = ds.Calculate(x);
+                double wait = 0.833;
+                Assert.AreEqual(wait, res);
+            }
+
+            [TestMethod]
+            public void TestMethod3()
+            {
+                DataService ds = new DataService();
+                double x = -2;
+                double res = ds.Calculate(x);
+                double wait = 0.64;
+                Assert.AreEqual(wait, res);
+            }
+
+            [TestMethod]
+            public void TestMethod4()
+            {
+                DataService ds = new DataService();
+                double x = -40;
+                double res = ds.Calculate(x);
+                double wait = -439.975;
+                Assert.AreEqual(wait, res);
+            }
+        }
+    }
---
+++ Tyuiu.AroyanAV.Sprint2.Task3.V21/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.AroyanAV.Sprint2.Task3.V21.Lib;
+
+
+namespace Tyuiu.AroyanAV.Sprint2.Task3.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнила: Ароян А. В. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if - полная и краткая форма записи                       *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Ароян Армине Вачагановна | ИСТНб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* значение переменной X с клавиатуры. Округлить полученное значение до    *");
+            Console.WriteLine("* трех знаков после запятой;                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.Write("Введите X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            
+            DataService ds = new DataService();
+            
+            double res = ds.Calculate(x);
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
